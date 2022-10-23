9fdf75435894220ef8cd43a568b609333654fef2
hasemasa1984 <hasemasa0719@gmail.com>
2022-10-23 22:07:01 +0900

    activehushの定義

---
+++ app/models/job.rb
@@ -0,0 +1,14 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '排泄介助' },
+    { id: 3, name: '入浴介助' },
+    { id: 4, name: '食事介助' },
+    { id: 5, name: '服薬介助' },
+    { id: 6, name: 'コール受電' },
+    { id: 7, name: '傾聴' },
+    { id: 8, name: '雑務' },
+    { id: 9, name: '記録' },
+    { id: 10, name: 'その他' }
+  ]
+  end
--- db/migrate/20221023124224_create_visuals.rb
+++ db/migrate/20221023124224_create_visuals.rb
@@ -3,6 +3,7 @@ class CreateVisuals < ActiveRecord::Migration[6.0]
     create_table :visuals do |t|
       t.references :user, null: false, foreign_key: true
       t.datetime :datetime, null: false
+      t.integer    :job_id     , null: false
       t.timestamps
     end
   end
