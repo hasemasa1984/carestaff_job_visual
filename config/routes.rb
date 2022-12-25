aebc54ae000918037f6c839c9000246fa2cfa9ba
hasemasa1984 <107299485+hasemasa1984@users.noreply.github.com>
2022-12-25 21:02:34 +0900

    createアクションが作動するようにcreateのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'visuals#index'
-  resources :visuals, only: [:index,:new,:show,:edit]
+  resources :visuals, only: [:index,:new,:create,:show,:edit]
 end
